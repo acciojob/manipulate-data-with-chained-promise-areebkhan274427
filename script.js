95a1c37fa28c658f5bb817880dd8c748a1f8ec09
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-03-27 02:45:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 //your JS code here. If required.
+var arr=[1,2,3,4];
+
+function print(){
+	return new Promise(function(resolve,reject){
+			resolve(arr);
+	});
+}
+
+function odd(){
+	return new Promise(function(resolve){
+		setTimeout(()=>{
+			arr=arr.filter(function(num){
+				return num%2==0;
+			});
+			document.getElementById('output').innerHTML=arr;
+			resolve(arr);
+		},1000);
+	})
+}
+
+function even(){
+	return new Promise(function(resolve){
+	
+		setTimeout(()=>{
+			arr.forEach(function(ele,index){
+				
+				arr[index]=arr[index]*2;
+			})
+			document.getElementById('output').innerHTML=arr;
+			resolve(arr);
+		},2000);
+	})
+}
+
+
+print().then(function(data){
+	return odd(data);
+}).then(function(data){
+	return even(data);
+});
+
