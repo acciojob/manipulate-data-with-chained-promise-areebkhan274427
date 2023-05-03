b8c86770b44403223e0e95fee102e2ae50d96c65
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-05-03 11:14:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,30 @@
 //your JS code here. If required.
-var arr=[1,2,3,4];
-
-function print(){
-	return new Promise(function(resolve,reject){
-			resolve(arr);
-	});
+const arr = [1, 2, 3, 4];
+function promise(arr, time) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(arr);
+    }, time);
+  });
 }
+const op = document.getElementById("output");
+promise(arr, 3000)
+  .then((res) => {
+    const evenArr = res.filter((num) => {
+      return num % 2 === 0;
+    });
+    return promise(evenArr, 1000);
+  })
+  .then((res) => {
 
-function odd(){
-	return new Promise(function(resolve){
-		setTimeout(()=>{
-			arr=arr.filter(function(num){
-				return num%2==0;
-			});
-			document.getElementById('output').innerHTML=arr;
-			resolve(arr);
-		},1000);
-	})
-}
+    op.innerText = res;
+    const multiplyBy2 = res.map((num) => {
+      return num * 2;
+    });
 
-function even(){
-	return new Promise(function(resolve){
-	
-		setTimeout(()=>{
-			arr.forEach(function(ele,index){
-				
-				arr[index]=arr[index]*2;
-			})
-			document.getElementById('output').innerHTML=arr;
-			resolve(arr);
-		},2000);
-	})
-}
-
-
-print().then(function(data){
-	return odd(data);
-}).then(function(data){
-	return even(data);
-});
+    return promise(multiplyBy2, 2000);
+  })
+  .then((res) => {
+    op.innerText = res;
+  });
 
